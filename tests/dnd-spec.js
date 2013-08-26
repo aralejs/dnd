dc11a3de9b5143fb175f8c51f3460f879cf1b447
ustccjw <317713370@qq.com>
2013-08-26 10:21:20 +0800

    modify tests

--- tests/dnd-spec.js
+++ tests/dnd-spec.js
@@ -51,10 +51,20 @@ define(function(require){
           
           // 不合法配置mousedown后, 自动转化为默认配置
           dnd = new Dnd('#drag1', {containment: '#drop1', proxy: '#drag1',
-                drop: '#drag1'}) ;
+                drop: '#drag1', axis: 0, visible: 0, revert: 0,      
+                revertDuration:'aaa', disabled:0, dragCursor:0, dropCursor:0, 
+                zIndex: 'aaa'}) ;
           expect(dnd.get('containment')).to.be('#drop1') ;
           expect(dnd.get('proxy')).to.be('#drag1') ;
           expect(dnd.get('drop')).to.be('#drag1') ;
+          expect(dnd.get('axis')).to.be(0) ;
+          expect(dnd.get('visible')).to.be(0) ;
+          expect(dnd.get('revert')).to.be(0) ;
+          expect(dnd.get('revertDuration')).to.be('aaa') ;
+          expect(dnd.get('disabled')).to.be(0) ;
+          expect(dnd.get('dragCursor')).to.be(0) ;
+          expect(dnd.get('dropCursor')).to.be(0) ;
+          expect(dnd.get('zIndex')).to.be('aaa') ;
           $('#drag1').on('mousedown', function(event){
               event.which = 1 ;
           }) ;
@@ -65,6 +75,14 @@ define(function(require){
               expect(dnd.get('proxy').get(0) !==   
                       $('#drag1').get(0)).to.be(true) ;
               expect(dnd.get('drop')).to.be(null) ;
+              expect(dnd.get('axis')).to.be(false) ;
+              expect(dnd.get('visible')).to.be(false) ;
+              expect(dnd.get('revert')).to.be(false) ;
+              expect(dnd.get('revertDuration')).to.be(500) ;
+              expect(dnd.get('disabled')).to.be(false) ;
+              expect(dnd.get('dragCursor')).to.be('move') ;
+              expect(dnd.get('dropCursor')).to.be('copy') ;
+              expect(dnd.get('zIndex')).to.be(9999) ;
               done() ;
           }, 80) ;
       });
@@ -83,49 +101,16 @@ define(function(require){
               y = event.pageY = originy + 20 ;
           }) ;
           $('#drag1').on('mousemove', function(event){
-              event.pageX = x + 10 ;
-              event.pageY = y + 10 ;
-          }) ;
-          
-          $('#drag1').trigger('mousedown') ;
-          $('#drag1').trigger('mousemove') ;
-          $(document).trigger('mouseup') ;
-          setTimeout(function(){
-              expect($('#drag1').offset().left).to.be(originx + 10) ;
-              expect($('#drag1').offset().top).to.be(originy + 10) ;
-              done() ;
-          }, 80) ;
-      }) ;
-      
-      
-      it('dataTransfer', function(done){
-          var originx = $('#drag1').offset().left ;
-              originy = $('#drag1').offset().top ;
-              x = 0,
-              y = 0 ;
-          
-          dnd = new Dnd('#drag1', {drop: '#drop1'}) ;
-          $('#drag1').on('mousedown', function(event){
-              event.which = 1 ;
-              x = event.pageX = originx + 20 ;
-              y = event.pageY = originy + 20 ;
-          }) ;
-          $('#drag1').on('mousemove', function(event){
               event.pageX = x + 200 ;
               event.pageY = y + 300 ;
           }) ;
-          dnd.on('dragstart', function(dataTransfer, dragging, dropping){
-              dataTransfer.data = '我是叁儿' ;
-          }) ;
-          dnd.on('drop', function(dataTransfer, dragging, dropping){
-              dropping.data('message', dataTransfer.data);
-          }) ;
           
           $('#drag1').trigger('mousedown') ;
           $('#drag1').trigger('mousemove') ;
           $(document).trigger('mouseup') ;
           setTimeout(function(){
-              expect($('#drop1').data('message')).to.be('我是叁儿') ;
+              expect($('#drag1').offset().left).to.be(originx + 200) ;
+              expect($('#drag1').offset().top).to.be(originy + 300) ;
               done() ;
           }, 80) ;
       }) ;
@@ -147,46 +132,113 @@ define(function(require){
               event.pageX = x + 1000 ;
               event.pageY = y + 1000 ;
           }) ;
-          
+              
           $('#drag1').trigger('mousedown') ;
           $('#drag1').trigger('mousemove') ;
           $(document).trigger('mouseup') ;
           setTimeout(function(){
               expect($('#drag1').offset().left + $('#drag1').outerWidth()).to.
-                      be($('#container1').offset().left +          
+                      be($('#container1').offset().left +      
                       $('#container1').outerWidth()) ;
               expect($('#drag1').offset().top).to.
                       be($('#container1').offset().top) ;
               done() ;
           }, 80) ;
-      }) ;  
+      }) ; 
       
-     
-      it('revert', function(done){
+      
+      it('close', function(done){
           var originx = $('#drag1').offset().left ;
               originy = $('#drag1').offset().top ;
               x = 0,
               y = 0 ;
           
-          dnd = new Dnd('#drag1', {revert: true}) ;
+          dnd = new Dnd('#drag1') ;
           $('#drag1').on('mousedown', function(event){
               event.which = 1 ;
               x = event.pageX = originx + 20 ;
               y = event.pageY = originy + 20 ;
           }) ;
           $('#drag1').on('mousemove', function(event){
-              event.pageX = x + 100 ;
-              event.pageY = y + 100 ;
+              event.pageX = x + 10 ;
+              event.pageY = y + 10 ;
           }) ;
           
+          dnd.close() ;
           $('#drag1').trigger('mousedown') ;
           $('#drag1').trigger('mousemove') ;
           $(document).trigger('mouseup') ;
           setTimeout(function(){
               expect($('#drag1').offset().left).to.be(originx) ;
               expect($('#drag1').offset().top).to.be(originy) ;
+              dnd.open() ;
               done() ;
           }, 80) ;
+      }) ;
+      
+      
+      it('dataTransfer', function(done){
+          var originx = $('#drag1').offset().left ;
+              originy = $('#drag1').offset().top ;
+              x = 0,
+              y = 0 ;
+          
+          dnd = new Dnd('#drag1', {drop: '#drop1'}) ;
+          $('#drag1').on('mousedown', function(event){
+              event.which = 1 ;
+              x = event.pageX = originx + 20 ;
+              y = event.pageY = originy + 20 ;
+          }) ;
+          $('#drag1').on('mousemove', function(event){
+              event.pageX = x + 200 ;
+              event.pageY = y + 220 ;
+          }) ;
+          dnd.on('dragstart', function(dataTransfer, dragging, dropping){
+              dataTransfer.data = '我是叁儿' ;
+          }) ;
+          dnd.on('drop', function(dataTransfer, dragging, dropping){
+              dropping.data('message', dataTransfer.data);
+          }) ;
+          
+          $('#drag1').trigger('mousedown') ;
+          $('#drag1').trigger('mousemove') ;
+          $(document).trigger('mouseup') ;
+          setTimeout(function(){
+              expect($('#drop1').data('message')).to.be('我是叁儿') ;
+              done() ;
+          }, 80) ;
+      }) ;
+      
+     
+      it('esc revert', function(done){
+          var originx = $('#drag1').offset().left ;
+              originy = $('#drag1').offset().top ;
+              x = 0,
+              y = 0 ;
+          
+          dnd = new Dnd('#drag1', {drop:'#drop1', revert: true}) ;
+          $('#drag1').on('mousedown', function(event){
+              event.which = 1 ;
+              x = event.pageX = originx + 20 ;
+              y = event.pageY = originy + 20 ;
+          }) ;
+          $('#drag1').on('mousemove', function(event){
+              event.pageX = x + 200 ;
+              event.pageY = y + 300 ;
+          }) ;
+          $('#drag1').on('keydown', function(event){
+              event.which = 27 ;
+          }) ;
+          
+          $('#drag1').trigger('mousedown') ;
+          $('#drag1').trigger('mousemove') ;
+          $('#drag1').trigger('keydown') ;
+          $(document).trigger('mouseup') ;
+          setTimeout(function(){
+              expect($('#drag1').offset().left).to.be(originx) ;
+              expect($('#drag1').offset().top).to.be(originy) ;
+              done() ;
+          }, 1000) ;
       }) ; 
       
   });
